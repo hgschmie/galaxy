f1f4fa7424c66dd4197070333f3ff54f5822e56e
Josh Toft <joshtoft@gmail.com>
2012-03-22 14:49:51 -0700

    Filter spacing fix

--- lib/galaxy/filter.rb
+++ lib/galaxy/filter.rb
@@ -1,50 +1,51 @@
 module Galaxy
-    module Filter
-        def self.new args
-            filters = []
+  module Filter
+    def self.new args
+      filters = []
 
-            case args[:set]
-                when :all, "all"
-                    filters << lambda { true }
-              when :none, "none"
-                  filters << lambda { false }
-                when :empty, "empty"
-                    filters << lambda { |a| a.config_path.nil? }
-                when :taken, "taken"
-                    filters << lambda { |a| a.config_path }
-            end
+      case args[:set]
+      when :all, "all"
+        filters << Proc.new { true }
+      when :none, "none"
+        filters << Proc.new { false }
+      when :empty, "empty"
+        filters << Proc.new { |a| a.config_path.nil? }
+      when :taken, "taken"
+        filters << Proc.new { |a| a.config_path }
+      end
 
-            if args[:env] || args[:version] || args[:type]
-                env = args[:env] || "[^/]+"
-                version = args[:version] || "[^/]+"
-                type = args[:type] || ".+"
+      if args[:env] || args[:version] || args[:type]
+        env = args[:env] || "[^/]+"
+        version = args[:version] || "[^/]+"
+        type = args[:type] || ".+"
 
-                filters << lambda { |a| a.config_path =~ %r!^/#{env}/#{version}/#{type}$! }
-            end
+        filters << Proc.new { |a| a.config_path =~ %r!^/#{env}/#{version}/#{type}$! }
+      end
+      
+      if args[:agent_id]
+        filters << lambda { |a| a.agent_id == args[:agent_id] }
+      end
+      
+      if args[:agent_group]
+        filters << lambda { |a| a.agent_group == args[:agent_group] }
+      end
+      
+      if args[:machine]
+        filters << lambda { |a| a.machine == args[:machine] }
+      end
+      
+      if args[:state]
+        filters << lambda { |a| a.status == args[:state] }
+      end
+      
+      if args[:agent_state]
+        filters << lambda { |a| a.agent_status == args[:agent_state] }
+      end
 
-            if args[:agent_id]
-                filters << lambda { |a| a.agent_id == args[:agent_id] }
-            end
-
-            if args[:agent_group]
-                filters << lambda { |a| a.agent_group == args[:agent_group] }
-            end
-
-            if args[:machine]
-                filters << lambda { |a| a.machine == args[:machine] }
-            end
-
-            if args[:state]
-                filters << lambda { |a| a.status == args[:state] }
-            end
-
-            if args[:agent_state]
-                filters << lambda { |a| a.agent_status == args[:agent_state] }
-            end
-
-            lambda do |a|
-                filters.inject(true) { |result, filter| result && filter.call(a) }
-            end
-        end
+      lambda do |a|
+        filters.inject(true) { |result, filter| result && filter.call(a) }
+      end
     end
+    
+  end
 end
