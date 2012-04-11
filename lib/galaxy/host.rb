25e79ccfa55033db3608dac085d266ef9f7ca29e
Josh Toft <joshtoft@gmail.com>
2012-04-11 12:04:51 -0700

    Wrapped class methods in `class << self` block

--- lib/galaxy/host.rb
+++ lib/galaxy/host.rb
@@ -2,9 +2,9 @@ require 'tempfile'
 require 'syslog'
 require 'logger'
 
-module Galaxy
-  module HostUtils
-    def HostUtils.logger ident="galaxy"
+module Galaxy::HostUtils
+  class << self
+    def logger ident="galaxy"
       @logger ||= begin
         log = Syslog.open ident, Syslog::LOG_PID | Syslog::LOG_CONS, Syslog::LOG_LOCAL7
         class << log
@@ -16,21 +16,21 @@ module Galaxy
           alias_method :unsafe_debug, :debug
 
           def debug * args
-            args.length == 1 ? unsafe_debug(safe_format(args[0])) : unsafe_debug(* args)
+            args.length == 1 ? unsafe_debug(safe_format(args.first)) : unsafe_debug(* args)
           end
 
           alias_method :unsafe_info, :info
 
           def info * args
-            args.length == 1 ? unsafe_info(safe_format(args[0])) : unsafe_info(* args)
+            args.length == 1 ? unsafe_info(safe_format(args.first)) : unsafe_info(* args)
           end
 
           def warn * args
-            args.length == 1 ? warning(safe_format(args[0])) : warning(* args)
+            args.length == 1 ? warning(safe_format(args.first)) : warning(* args)
           end
 
           def error * args
-            args.length == 1 ? err(safe_format(args[0])) : err(* args)
+            args.length == 1 ? err(safe_format(args.first)) : err(* args)
           end
 
           # set log levels from standard Logger levels
@@ -68,7 +68,7 @@ module Galaxy
     # This implementation tries +who am i+, available on some unix platforms, to check the owner of the controlling terminal,
     # which preserves ownership across +su+ and +sudo+. Failing that, the environment is checked for a +USERNAME+ or +USER+ variable.
     # Finally, the system password database is consulted.
-    def HostUtils.shell_user
+    def shell_user
       guesses = []
       guesses << `who am i 2> /dev/null`.split[0]
       guesses << ENV['USERNAME']
@@ -77,15 +77,15 @@ module Galaxy
       guesses.first { |guess| notguess.nil? and notguess.empty? }
     end
 
-    def HostUtils.avail_path
+    def avail_path
       @avail_path ||= begin
         directories = %w{ /usr/local/var/galaxy /var/galaxy /var/tmp /tmp }
         directories.find { |dir| FileTest.writable? dir }
       end
     end
 
-    def HostUtils.tar
-      @tar ||= begin
+    def tar
+      @tar ||= begin        
         unless `which gtar` =~ /^no gtar/ || `which gtar`.length == 0
           "gtar"
         else
@@ -94,7 +94,7 @@ module Galaxy
       end
     end
 
-    def HostUtils.switch_user user
+    def switch_user user
       pwent = Etc::getpwnam(user)
       uid, gid = pwent.uid, pwent.gid
       if Process.gid != gid or Process.uid != uid
@@ -107,29 +107,29 @@ module Galaxy
       end
     end
 
-    class CommandFailedError < Exception
-      attr_reader :command, :exitstatus, :output
-
-      def initialize command, exitstatus, output
-        @command = command
-        @exitstatus = exitstatus
-        @output = output
-      end
-
-      def message
-        "Command '#{@command}' exited with status code #{@exitstatus} and output: #{@output.join("\n")}".chomp()
-      end
-    end
-
     # An alternative to Kernel.system that invokes a command, raising an exception containing
     # the command's stdout and stderr if the command returns a status code other than 0
-    def HostUtils.system command
+    def system command
       output = IO.popen("#{command} 2>&1") { |io| io.readlines }
       unless $?.success?
         raise CommandFailedError.new(command, $?.exitstatus, output)
       end
       output
     end
-  
   end
+  
+  class CommandFailedError < Exception
+    attr_reader :command, :exitstatus, :output
+
+    def initialize command, exitstatus, output
+      @command = command
+      @exitstatus = exitstatus
+      @output = output
+    end
+
+    def message
+      "Command '#{@command}' exited with status code #{@exitstatus} and output: #{@output.join("\n")}".chomp()
+    end
+  end
+    
 end
